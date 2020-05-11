b9fd1e43a1b077edea51c0419d5523d94fe7651e
tao <tao.you@vanderbilt.edu>
2020-05-11 13:48:14 -0500

    Updated default theme to be v3.2 delivered default theme.

--- default/js/main.js
+++ default/js/main.js
@@ -1,37 +1,62 @@
 /**
  * @file plugins/themes/default/js/main.js
  *
- * Copyright (c) 2014-2017 Simon Fraser University
- * Copyright (c) 2000-2017 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2014-2020 Simon Fraser University
+ * Copyright (c) 2000-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @brief Handle JavaScript functionality unique to this theme.
  */
 (function($) {
 
-	// Update nav menu ARIA states on focus, blur and mouse over/out events
-	$('.navDropdownMenu ul').on('focus.default  mouseenter.default', '[aria-haspopup="true"]', function (e) {
-		$(e.currentTarget).attr('aria-expanded', true);
-	});
+	// Initialize dropdown navigation menus on large screens
+	// See bootstrap dropdowns: https://getbootstrap.com/docs/4.0/components/dropdowns/
+	if (typeof $.fn.dropdown !== 'undefined') {
+		var $nav = $('#navigationPrimary, #navigationUser'),
+		$submenus = $('ul', $nav);
+		function toggleDropdowns() {
+			if (window.innerWidth > 992) {
+				$submenus.each(function(i) {
+					var id = 'pkpDropdown' + i;
+					$(this)
+						.addClass('dropdown-menu')
+						.attr('aria-labelledby', id);
+					$(this).siblings('a')
+						.attr('data-toggle', 'dropdown')
+						.attr('aria-haspopup', true)
+						.attr('aria-expanded', false)
+						.attr('id', id)
+						.attr('href', '#');
+				});
+				$('[data-toggle="dropdown"]').dropdown();
 
-	$('.navDropdownMenu ul').on('blur.default  mouseleave.default', '[aria-haspopup="true"]', function (e) {
-		$(e.currentTarget).attr('aria-expanded', false)
-			.attr('pkp-touch-state', '');
-	});
-
-	// Taps on nav menu items with submenus should expand on first tap
-	$('.navDropdownMenu a').on('touchstart', function(e) {
-		var target = $(e.target),
-			li = target.parent('li');
-		if (li.length && li.attr('aria-haspopup')) {
-			var state = li.attr('pkp-touch-state') || '';
-			if (state != 'open') {
-				target.trigger('focus.default');
-				li.attr('pkp-touch-state', 'open');
-				e.preventDefault();
+			} else {
+				$('[data-toggle="dropdown"]').dropdown('dispose');
+				$submenus.each(function(i) {
+					$(this)
+						.removeClass('dropdown-menu')
+						.removeAttr('aria-labelledby');
+					$(this).siblings('a')
+						.removeAttr('data-toggle')
+						.removeAttr('aria-haspopup')
+						.removeAttr('aria-expanded',)
+						.removeAttr('id')
+						.attr('href', '#');
+				});
 			}
 		}
-	})
+		window.onresize = toggleDropdowns;
+		$().ready(function() {
+			toggleDropdowns();
+		});
+	}
+
+	// Toggle nav menu on small screens
+	$('.pkp_site_nav_toggle').click(function(e) {
+  		$('.pkp_site_nav_menu').toggleClass('pkp_site_nav_menu--isOpen');
+  		$('.pkp_site_nav_toggle').toggleClass('pkp_site_nav_toggle--transform');
+	});
+
 
 	// Register click handlers for the search panel
 	var headerSearchPanelIsClosing = false,
@@ -117,6 +142,20 @@
 		e.chartOptions.elements.line.backgroundColor = 'rgba(0, 122, 178, 0.6)';
 		e.chartOptions.elements.rectangle.backgroundColor = 'rgba(0, 122, 178, 0.6)';
 	});
+
+	// Toggle display of consent checkboxes in site-wide registration
+	var $contextOptinGroup = $('#contextOptinGroup');
+	if ($contextOptinGroup.length) {
+		var $roles = $contextOptinGroup.find('.roles :checkbox');
+		$roles.change(function() {
+			var $thisRoles = $(this).closest('.roles');
+			if ($thisRoles.find(':checked').length) {
+				$thisRoles.siblings('.context_privacy').addClass('context_privacy_visible');
+			} else {
+				$thisRoles.siblings('.context_privacy').removeClass('context_privacy_visible');
+			}
+		});
+	}
 
 	// Initialize tag-it components
 	//
